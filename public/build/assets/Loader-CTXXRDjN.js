4be5df41a2d88fa7db95caeb799369c683b02726
Magicienrci <serge.touvoli@inphb.ci>
2024-11-09 10:04:08 +0000

    Ajout plus de total

--- public/build/assets/Loader-CTXXRDjN.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-Dpq9Pdya.js";function e(){return r.jsx("div",{className:"loader-container",children:r.jsx("div",{className:"loader"})})}export{e as L};
---
+++ public/build/assets/Loader-Deq9e3bC.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-UPZw1QB4.js";function e(){return r.jsx("div",{className:"loader-container",children:r.jsx("div",{className:"loader"})})}export{e as L};
